22b6b800d85be286b2a71ba62b284cb417505ee1
watsy0007 <watsy0007@gmail.com>
2016-10-21 22:06:17 +0800

    feature [docs] code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'crawl_station'
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
