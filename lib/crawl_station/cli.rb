a381dc686672b805cb8a50e5357a76afce8568dd
watsy0007 <watsy0007@gmail.com>
2016-10-25 20:44:49 +0800

    test(command): update cli

--- lib/crawl_station/cli.rb
+++ lib/crawl_station/cli.rb
@@ -2,6 +2,22 @@ require 'crawl_station/ruby_version_check'
 Signal.trap('INT') { puts; exit(1) }
 require 'crawl_station'
 
-if ARGV.first == 'create'
-  CrawlStation::Command.invoke(ARGV.shift, ARGV)
+puts ARGV
+if ARGV.size > 0
+  command = ARGV.shift
+
+  aliases = {
+    'new' => 'create',
+    'g' => 'generate'
+  }
+  command = aliases[command] || command
+  return CrawlStation::Command.invoke(command, ARGV)
+end
+
+%w(
+  create
+  generate
+).each do |command|
+  klass = "#{CrawlStation::Command}::#{command.capitalize}".camelize.constanize
+  puts klass.methods
 end
